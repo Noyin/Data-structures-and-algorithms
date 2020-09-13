9510a278377b17e913b83cffacee5d7f0b2c2366
noyin <noyin_lawal@hotmail.com>
2020-09-13 09:03:16 -0300

    Added Disjoint set union

---
+++ DisjointSetUnion.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+class DSU {
+    var parent: [Int]
+    var rank: [Int]
+    var size: Int {
+        return rank.max() ?? 0
+    }
+    
+    init(_ n: Int) {
+        self.parent = Array(0...(n + 1))
+        self.rank = Array(repeating: 1, count: n + 1)
+    }
+    
+    func find(_ a: Int) -> Int {
+        if a == parent[a] {
+            return a
+        }
+        
+        return find(parent[a])
+    }
+    
+    func union(_ a: Int, _ b: Int) -> Bool {
+        var i = find(a)
+        var j = find(b)
+        if i != j {
+            if rank[i] < rank[j] {
+                var temp = i
+                i = j
+                j = temp
+            }
+            
+            parent[j] = parent[i]
+            rank[j] += rank[i]
+            rank[i] = rank[j]
+            return true
+        }
+        
+        return false
+    }
+}
