e6453f0116936b0631dfa86bddf3fa11224d1d72
Noyin Lawal <Noyin@users.noreply.github.com>
2022-09-11 18:24:46 -0600

    Update SegmentTree.swift

--- SegmentTree.swift
+++ SegmentTree.swift
@@ -1,14 +1,14 @@
-class SegmentTree<T> {
-    private var maximum = [T]()
-    private var minimum = [T]()
-    private var array = [T]()
+class SegmentTree: CustomStringConvertible {
+    private var maximum = [Int]()
+    private var minimum = [Int]()
+    private var array = [Int]()
     
-    private init(_ array: [T]) {
-        self.array = array         
+    private init(_ array: [Int]) {
+        self.array = array
         build()
     }
 
-    class func make(_ array: [T]) -> SegmentTree  {
+    class func make(_ array: [Int]) -> SegmentTree  {
         return SegmentTree(array)
     }
     
@@ -18,9 +18,9 @@ class SegmentTree<T> {
         var tempMin = [array]
         
         while let lastMax = tempMax.last, let lastMin = tempMin.last, lastMax.count > 1 {
-            var currentMax = [T]()
-            var currentMin = [T]()
-            let count = last.count
+            var currentMax = [Int]()
+            var currentMin = [Int]()
+            let count = lastMax.count
         
             for i in 0..<count where i % 2 == 0 {
                 if i + 1 < count {
@@ -39,16 +39,55 @@ class SegmentTree<T> {
         let count = tempMax.count
         
         for i in (0..<count).reversed() {
-            maximuma.append(contentOf: tempMax[i])
-            minimum.append(contentOf: tempMin[i])
+            maximum.append(contentsOf: tempMax[i])
+            minimum.append(contentsOf: tempMin[i])
         }
     }
     
-    func getMin(_ start: Int, _ end: Int) -> T {
-        
+    func getMin(_ start: Int, _ end: Int) -> Int {
+        let count = array.count
+        return getMinHelper(start, end, 0, count - 1, 0)
     }
     
-    func getMax(_ start: Int, _ end: Int) -> T {
+    func getMinHelper(_ queryLow: Int, _ queryHigh: Int, _ low: Int, _ high: Int, _ pos: Int) -> Int {
+        if high < queryLow || queryHigh < low || pos >= minimum.count {
+            return Int.max
+        }
         
+        if queryLow <= low && high <= queryHigh {
+            return minimum[pos]
+        }
+        
+        
+        
+        
+        let mid = (low + high) >> 1
+        
+        return min(getMinHelper(queryLow, queryHigh, low, mid, (2 * pos) + 1),
+                  getMinHelper(queryLow, queryHigh, mid + 1, high, (2 * pos) + 2))
+    }
+    
+    func getMax(_ start: Int, _ end: Int) -> Int {
+        let count = array.count
+        return getMaxHelper(start, end, 0, count - 1, 0)
+    }
+    
+    func getMaxHelper(_ queryLow: Int, _ queryHigh: Int, _ low: Int, _ high: Int, _ pos: Int) -> Int {
+        if high < queryLow || queryHigh < low || pos >= maximum.count {
+            return Int.min
+        }
+        
+        if queryLow <= low && high <= queryHigh {
+            return maximum[pos]
+        }
+        
+        let mid = (low + high) >> 1
+        
+        return max(getMaxHelper(queryLow, queryHigh, low, mid, (2 * pos) + 1),
+                  getMaxHelper(queryLow, queryHigh, mid + 1, high, (2 * pos) + 2))
+    }
+
+    var description: String {
+        return "maximum: \(maximum) \nminimum: \(minimum)"
     }
 }
