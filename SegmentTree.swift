eee5d5772b58708d0a0ee157c2cb67a968910347
Noyin Lawal <Noyin@users.noreply.github.com>
2022-09-11 22:21:58 -0600

    Update SegmentTree.swift

--- SegmentTree.swift
+++ SegmentTree.swift
@@ -1,6 +1,8 @@
 class SegmentTree: CustomStringConvertible {
     private var maximum = [Int]()
     private var minimum = [Int]()
+    private var lazyMaximum = [Int]()
+    private var lazyMinimum = [Int]()
     private var array = [Int]()
     
     private init(_ array: [Int]) {
@@ -42,6 +44,9 @@ class SegmentTree: CustomStringConvertible {
             maximum.append(contentsOf: tempMax[i])
             minimum.append(contentsOf: tempMin[i])
         }
+        
+        lazyMaximum = Array(repeating: 0, count: maximum.count)
+        lazyMinimum = Array(repeating: 0, count: minimum.count)
     }
     
     func getMin(_ start: Int, _ end: Int) -> Int {
@@ -49,7 +54,7 @@ class SegmentTree: CustomStringConvertible {
         return getMinHelper(start, end, 0, count - 1, 0)
     }
     
-    func getMinHelper(_ queryLow: Int, _ queryHigh: Int, _ low: Int, _ high: Int, _ pos: Int) -> Int {
+    private func getMinHelper(_ queryLow: Int, _ queryHigh: Int, _ low: Int, _ high: Int, _ pos: Int) -> Int {
         if high < queryLow || queryHigh < low || pos >= minimum.count {
             return Int.max
         }
@@ -57,14 +62,66 @@ class SegmentTree: CustomStringConvertible {
         if queryLow <= low && high <= queryHigh {
             return minimum[pos]
         }
-        
-        
-        
-        
+
         let mid = (low + high) >> 1
         
         return min(getMinHelper(queryLow, queryHigh, low, mid, (2 * pos) + 1),
                   getMinHelper(queryLow, queryHigh, mid + 1, high, (2 * pos) + 2))
+    }
+    
+    func incrementMin(_ start: Int, _ end: Int, _ delta: Int) {
+        let count = array.count
+        return incrementMinHelper(start, end, 0, count - 1, 0, delta)
+    }
+    
+    private func incrementMinHelper(_ start: Int, _ end: Int, _ low: Int, _ high: Int, _ pos: Int, _ delta: Int) {
+        let count = minimum.count
+        if low > high || pos >= count { return }
+        
+        if lazyMinimum[pos] != 0 {
+            minimum[pos] += lazyMinimum[pos]
+            if low != high {
+                if (2 * pos) + 1 < count {
+                    lazyMinimum[(2 * pos) + 1] += lazyMinimum[pos]
+                }
+
+                if (2 * pos) + 2 < count {
+                    lazyMinimum[(2 * pos) + 2] += lazyMinimum[pos]
+                }
+            }
+            
+            lazyMinimum[pos] = 0
+        }
+        
+        if high < start || end < low {
+            return
+        }
+    
+        if start <= low && high <= end {
+            minimum[pos] += delta
+            if low != high {
+                if (2 * pos) + 1 < count {
+                    lazyMinimum[(2 * pos) + 1] += delta
+                }
+
+                if (2 * pos) + 2 < count {
+                    lazyMinimum[(2 * pos) + 2] += delta
+                }
+            }
+            return
+        }
+        
+        let mid = (low + high) >> 1
+        incrementMinHelper(start, end, low, mid, (2 * pos) + 1, delta)
+        incrementMinHelper(start, end, mid + 1, high, (2 * pos) + 2, delta)
+        
+        if (2 * pos) + 1 < count {
+            minimum[pos] = min(minimum[pos], minimum[(2 * pos) + 1])
+        }
+        
+        if (2 * pos) + 2 < count {
+            minimum[pos] = min(minimum[pos], minimum[(2 * pos) + 2])
+        }
     }
     
     func getMax(_ start: Int, _ end: Int) -> Int {
@@ -72,7 +129,7 @@ class SegmentTree: CustomStringConvertible {
         return getMaxHelper(start, end, 0, count - 1, 0)
     }
     
-    func getMaxHelper(_ queryLow: Int, _ queryHigh: Int, _ low: Int, _ high: Int, _ pos: Int) -> Int {
+    private func getMaxHelper(_ queryLow: Int, _ queryHigh: Int, _ low: Int, _ high: Int, _ pos: Int) -> Int {
         if high < queryLow || queryHigh < low || pos >= maximum.count {
             return Int.min
         }
@@ -85,6 +142,61 @@ class SegmentTree: CustomStringConvertible {
         
         return max(getMaxHelper(queryLow, queryHigh, low, mid, (2 * pos) + 1),
                   getMaxHelper(queryLow, queryHigh, mid + 1, high, (2 * pos) + 2))
+    }
+    
+    func incrementMax(_ start: Int, _ end: Int, _ delta: Int) {
+        let count = array.count
+        return incrementMaxHelper(start, end, 0, count - 1, 0, delta)
+    }
+    
+    private func incrementMaxHelper(_ start: Int, _ end: Int, _ low: Int, _ high: Int, _ pos: Int, _ delta: Int) {
+        let count = maximum.count
+        if low > high || pos >= count { return }
+        
+        if lazyMaximum[pos] != 0 {
+            maximum[pos] += lazyMaximum[pos]
+            if low != high {
+                if (2 * pos) + 1 < count {
+                    lazyMaximum[(2 * pos) + 1] += lazyMaximum[pos]
+                }
+
+                if (2 * pos) + 2 < count {
+                    lazyMaximum[(2 * pos) + 2] += lazyMaximum[pos]
+                }
+            }
+            
+            lazyMaximum[pos] = 0
+        }
+        
+        if high < start || end < low {
+            return
+        }
+    
+        if start <= low && high <= end {
+            maximum[pos] += delta
+            if low != high {
+                if (2 * pos) + 1 < count {
+                    lazyMaximum[(2 * pos) + 1] += delta
+                }
+
+                if (2 * pos) + 2 < count {
+                    lazyMaximum[(2 * pos) + 2] += delta
+                }
+            }
+            return
+        }
+        
+        let mid = (low + high) >> 1
+        incrementMaxHelper(start, end, low, mid, (2 * pos) + 1, delta)
+        incrementMaxHelper(start, end, mid + 1, high, (2 * pos) + 2, delta)
+        
+        if (2 * pos) + 1 < count {
+            maximum[pos] = max(maximum[pos], maximum[(2 * pos) + 1])
+        }
+        
+        if (2 * pos) + 2 < count {
+            maximum[pos] = max(maximum[pos], maximum[(2 * pos) + 2])
+        }
     }
 
     var description: String {
