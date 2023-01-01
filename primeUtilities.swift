b10a7620be5a89f36ba67ade5230ddc3e1487be0
Noyin Lawal <Noyin@users.noreply.github.com>
2022-12-31 20:34:05 -0700

    Update and rename primeFactors.swift to primeUtilities.swift

--- primeUtilities.swift
+++ primeUtilities.swift
@@ -16,3 +16,10 @@
 
     return factors
 }
+
+func isPrime(_ n: Int) -> Bool {
+    guard n >= 2     else { return false }
+    guard n != 2     else { return true  }
+    guard n % 2 != 0 else { return false }
+    return !stride(from: 3, through: Int(sqrt(Double(n))), by: 2).contains { n % $0 == 0 }
+}
