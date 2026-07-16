31b68ca69419c90d89462fb9de83c0ab12fd6b31
Noyin Lawal <Noyin@users.noreply.github.com>
2026-07-15 20:16:11 -0600

    Refactor gcd function for better performance
    Refactor gcd function to improve efficiency and correctness.

--- GCD_LCM.swift
+++ GCD_LCM.swift
@@ -1,16 +1,18 @@
-func gcd(_ a:Int, _ b:Int) -> Int {
-    if a == b {
-        return a
-    }
-    else {
-        if a > b {
-            return gcd(a-b, b)
+func gcd(_ a: Int, _ b: Int) -> Int {
+        if a % b == 0 || b % a == 0 {
+            return min(a, b)
         }
-        else {
-            return gcd(a, b-a)
+
+        if a == b {
+            return a
         }
+
+        if a < b {
+            return gcd(a, b % a)
+        }
+
+        return gcd(a % b, b)
     }
-}
    
 func lcm(_ x: Int, _ y: Int) -> Int {
    return x / gcd(x, y) * y
