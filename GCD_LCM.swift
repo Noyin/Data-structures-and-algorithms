6cf15a43ffa10ef64f4200744561f8ea6e8200f3
Noyin Lawal <Noyin@users.noreply.github.com>
2026-07-15 20:16:59 -0600

    Refactor gcd function for clarity

--- GCD_LCM.swift
+++ GCD_LCM.swift
@@ -1,18 +1,18 @@
 func gcd(_ a: Int, _ b: Int) -> Int {
-        if a % b == 0 || b % a == 0 {
-            return min(a, b)
-        }
-
-        if a == b {
-            return a
-        }
-
-        if a < b {
-            return gcd(a, b % a)
-        }
-
-        return gcd(a % b, b)
+    if a % b == 0 || b % a == 0 {
+        return min(a, b)
     }
+    
+    if a == b {
+        return a
+    }
+    
+    if a < b {
+        return gcd(a, b % a)
+    }
+    
+    return gcd(a % b, b)
+}
    
 func lcm(_ x: Int, _ y: Int) -> Int {
    return x / gcd(x, y) * y
